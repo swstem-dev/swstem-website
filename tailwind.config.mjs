35fb0f7088d5265a24de42cb6a122a77d229f16e
Rob Copeland <robcopeland@fastmail.com>
2023-10-28 17:37:18 -0400

    adds site-header and hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+      colors: {
+        "primary-500": "#090962",
+        "secondary-500": "#E46F21",
+        black: "#282626",
+      },
+    },
+  },
+  plugins: [],
+};
